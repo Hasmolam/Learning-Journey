1d837940034f54d640f6d2efc7f6679dedb72e84
Hasmolam <hasanhuseyinyolcu25@gmail.com>
2025-10-29 16:44:29 +0300

    Update .gitignore and progress tracking; add C# programming examples

---
+++ Youtube/C# Programlama Eğitimi/example1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Kullanıcı tarafından girilen sayının aşağıdaki önergelere göre hesabını gerçekleştiren kodu geliştiriniz.
+
+            Console.Write("Lütfen bir sayı giriniz: ");
+            int sayi = int.Parse(Console.ReadLine());
+            // sayi < 3                    => sayi * 5
+            // sayi > 3 && sayi < 9        => sayi * 3
+            // sayi >= 9 && sayi % 2 == 0  => sayi * 10
+            // sayi % 2 == 1               => sayi
+            // hiçbiri değilse             => -1
+            int sonuc = sayi < 3 ? sayi * 5 : (sayi > 3 && sayi < 9 ? sayi * 3 : (sayi >= 9 && sayi % 2 == 0 ? sayi * 10 : (sayi % 2 == 1 ? sayi : -1)));
+            Console.WriteLine("Sonuç: " + sonuc);
+        }
+    }
+}
---
+++ Youtube/C# Programlama Eğitimi/example2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Reflection.Metadata;
+
+namespace App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Hava durumunu tutan string değişkeninin değerine göre aşağıdaki önergeleri uygulayan programı yazınız.
+            // "Yağmurlu" => "Şemsiye almalısın"
+            // "Güneşli" => "Bol bol d vitamini alman dileğiyle..."
+            // "Kapalı" => "Yağmur yağabilir"
+            string havaDurumu = "Kapalı";
+            Console.WriteLine(havaDurumu == "Yağmurlu" ? "Şemsiye almalısın": ( havaDurumu == "Güneşli" ? "Bol bol d vitamini alman dileğiyle...": "Yağmur yağabilir"));
+        }
+    }
+}
